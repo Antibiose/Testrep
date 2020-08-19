7b2854fabf0337f0b6b94287a2e3fd78d9c3b4d7
Antibiose <61875516+Antibiose@users.noreply.github.com>
2020-08-19 22:18:06 +0200

    Update hu.py

--- hu.py
+++ hu.py
@@ -1,1 +1,14 @@
 print("Hu")
+
+def mult_sum(number1, number2):
+    result = number1 * number2
+    if result <1000:
+        print(result)
+    else: 
+        result = number1 + number2
+        print(result)
+    
+number1=200
+number2=30
+
+ergebnis = mult_sum(number1, number2)
