8ceb26d3bef0401a9f47d0123cddf33ef13364f3
test <nsiddiqui@fastbill.com>
2020-08-19 22:19:10 +0200

    kommis dazu

--- hu.py
+++ hu.py
@@ -1,1 +1,17 @@
 print("Hu")
+
+def mult_sum(number1, number2):
+    result = number1 * number2
+    if result <1000:
+        print(result)
+    else: 
+        result = number1 + number2
+        print(result)
+    
+number1=200
+number2=30
+
+ergebnis = mult_sum(number1, number2)
+
+# random kommentar
+# random Kommentar 2
